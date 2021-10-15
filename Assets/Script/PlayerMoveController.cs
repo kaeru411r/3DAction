46dadd00f8f2b36c26128eea44e845b32ebc6e13
kaeru411r <ike269fr@gmail.com>
2021-10-15 13:09:23 +0900

    戦車が前後左右に動くように

---
+++ Assets/Script/PlayerMoveController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    [Tooltip("移動速度")]
+    [SerializeField] float m_speed;
+    /// <summary>プレイヤーのリジッドボディ</summary>
+    Rigidbody m_rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 vector = new Vector3(x, 0, z);
+        vector = vector.normalized;
+        m_rb.AddForce(vector, ForceMode.Impulse);
+    }
+}
