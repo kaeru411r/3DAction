9b8a86e09fd7069c8c029ccf4dcf67c83c3e3103
kaeru411r <ike269fr@gmail.com>
2021-12-09 22:42:06 +0900

    キャタピラ設定用の変数を用意

--- Assets/Script/CaterpillarController.cs
+++ Assets/Script/CaterpillarController.cs
@@ -8,34 +8,63 @@ public class CaterpillarController : MonoBehaviour
     [SerializeField] float _forwardTorque;
     [SerializeField] float _backTorque;
     [SerializeField] float _brakeTorque;
-    //[SerializeField] float _
+    [SerializeField] float _wheelDanpingRate;
+    [SerializeField] ForwardFriction _forwardWheelFriction;
+    [SerializeField] SideFriction _sideWheelFriction;
 
     // Start is called before the first frame update
     void Start()
     {
-        //_wheels[1].RightWheel.suspensionSpring.damper = 0;
+
     }
 
+    private void Reset()
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Move(Vector2 dir)
     {
         dir = dir.normalized;
-        
+
     }
 }
 
 [System.Serializable]
-public struct Wheels
+public class Wheels
 {
+    public float Mass;
+    public float Radius;
+    public float SuspensionDistance;
     public WheelCollider RightWheel;
     public WheelCollider LeftWheel;
     public float Spring;
     public float Damper;
     public float TargetPosition;
+    public Vector3 Center;
+}
 
+[System.Serializable]
+public class ForwardFriction
+{
+    public float ExtremumSlip;
+    public float ExtremumValue;
+    public float AsymptoteSlip;
+    public float AsymptoteValue;
+    public float Stiffness;
+}
+
+[System.Serializable]
+public class SideFriction
+{
+    public float ExtremumSlip;
+    public float ExtremumValue;
+    public float AsymptoteSlip;
+    public float AsymptoteValue;
+    public float Stiffness;
 }
