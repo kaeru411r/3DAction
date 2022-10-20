30a1a988d01588e04489445e5e27cee185b59b4b
kaeru411r <ike269fr@gmail.com>
2022-10-21 02:31:54 +0900

    プレイヤーの操作を制限するためのベースクラスを作った #53

---
+++ Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーが操作をするためのクラスのベースクラス
+/// プレイヤー操作が有効な時のみ各コンポーネントの関数を実行できる関数を持っている
+/// </summary>
+public abstract class PlayerBase : MonoBehaviour
+{
+    protected bool _isActive;
+
+    public bool IsActive { get => _isActive; set => _isActive = value; }
+
+    /// <summary>
+    /// 操作が有効な時のみメソッドを実行する
+    /// </summary>
+    /// <typeparam name="Input"></typeparam>
+    /// <typeparam name="Return"></typeparam>
+    /// <param name="value"></param>
+    /// <param name="method"></param>
+    /// <returns></returns>
+    protected Return Call<Input, Return>(Input value, Func<Input, Return> method) 
+    {
+        if (!_isActive)
+        {
+            return default;
+        }
+        return method(value);
+    }
+
+    /// <summary>
+    /// 操作が有効な時のみメソッドを実行する
+    /// </summary>
+    /// <typeparam name="Input"></typeparam>
+    /// <typeparam name="Return"></typeparam>
+    /// <param name="value"></param>
+    /// <param name="method"></param>
+    /// <returns></returns>
+    protected Return Call<Return>(Func<Return> method)
+    {
+        if (!_isActive)
+        {
+            return default;
+        }
+        return method();
+    }
+
+    /// <summary>
+    /// 操作が有効な時のみ変数に代入する
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    protected bool Set<T>(ref T a, in T b)
+    {
+        if (!_isActive)
+        {
+            return false;
+        }
+        a = b;
+        return true;
+    }
+}
